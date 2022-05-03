d80924d3b208134ed3812910ef4ace6632b0fc8c
khemiriahmed <khemiriahmed6@gmail.com>
2022-05-03 14:19:32 +0200

    Stream Angular App

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,24 +9,24 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getCarousel(): Observable<any> {
-    return this.http.get<any>('https://my-json-server.typicode.com/khemiriahmed/stream-app-angular/carousel');
+    return this.http.get<any>('https://my-json-server.typicode.com/khemiriahmed/stream-angular-app/carousel');
   }
 
   getMovie(): Observable<any> {
-    return this.http.get<any>('https://my-json-server.typicode.com/khemiriahmed/stream-app-angular/movie');
+    return this.http.get<any>('https://my-json-server.typicode.com/khemiriahmed/stream-angular-app/movie');
   }
 
   getUpcommingMovie(): Observable<any> {
-    return this.http.get<any>('https://my-json-server.typicode.com/khemiriahmed/stream-app-angular/upCommingMovie');
+    return this.http.get<any>('https://my-json-server.typicode.com/khemiriahmed/stream-angular-app/upCommingMovie');
   }
 
   
   getEpisode(): Observable<any> {
-    return this.http.get<any>('https://my-json-server.typicode.com/khemiriahmed/stream-app-angular/episode');
+    return this.http.get<any>('https://my-json-server.typicode.com/khemiriahmed/stream-angular-app/episode');
   }
 
   getpageMovies(): Observable<any> {
-    return this.http.get<any>('https://my-json-server.typicode.com/khemiriahmed/stream-app-angular/page-movies');
+    return this.http.get<any>('https://my-json-server.typicode.com/khemiriahmed/stream-angular-app/page-movies');
   }
 
   
